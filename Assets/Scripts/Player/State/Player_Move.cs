dae0d0d62c7f7cbcc8b2b13d805b86c2e63d0669
reso1ute9 <warobot1997@gmail.com>
2023-04-08 23:54:57 +0800

    add player move

---
+++ Assets/Scripts/Player/State/Player_Move.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 玩家移动状态
+public class Player_Move : PlayerStateBase
+{
+    public override void Enter() {
+        base.Enter();
+        // this.PlayAnimation("Idle");
+    }
+
+    public override void Exit() {
+        base.Exit();
+    }
+
+    public override void Update() {
+        base.Update();
+    }
+}
