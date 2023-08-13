b0faec2b2807b4c8731d0c2d05b5d18c3b573e2d
ZhangGuoxin <warobot1997@gmail.com>
2023-08-13 23:07:52 +0800

    added tree hit logic and animation

--- Assets/Scripts/GameScene/MapObject/Tree_Controller.cs
+++ Assets/Scripts/GameScene/MapObject/Tree_Controller.cs
@@ -1,10 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using JKFrame;
 
 public class Tree_Controller : MapObjectBase {
-    public void Hurt(float damage) {
-        UnityEngine.Debug.Log("树受伤了:" + damage);
+    [SerializeField] private Animator animator;
+    [SerializeField] private AudioClip[] hurtAudioClips;
+    [SerializeField] private float maxHp;
+    private float hp;
+    
+
+    public void Start() {
+        hp = maxHp;
     }
 
+    public void Hurt(float damage) {
+        UnityEngine.Debug.Log("树受伤了:" + damage);
+        hp -= damage;
+        if (hp <= 0) {
+            Dead();
+        } else {
+            animator.SetTrigger("Hurt");
+            AudioManager.Instance.PlayOnShot(
+                hurtAudioClips[Random.Range(0, hurtAudioClips.Length)],
+                transform.position
+            );
+        }
+    }
+
+    private void Dead() {
+        // TODO: 临时直接销毁
+        Destroy(gameObject);
+    }
 }
