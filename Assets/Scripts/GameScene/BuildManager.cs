6ed37b3f82cb72927f402396ed8088b3ac8078ea
ZhangGuoxin <warobot1997@gmail.com>
2023-09-10 23:44:38 +0800

    Add building preview colors

--- Assets/Scripts/GameScene/BuildManager.cs
+++ Assets/Scripts/GameScene/BuildManager.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using JKFrame;
+using System;
 
 public class BuildManager : SingletonMono<BuildManager>
 {
+    [SerializeField]
+    private float virtualCellSize = 0.25f;
     private Dictionary<string, BuildingBase> buildPreviewDict = new Dictionary<string, BuildingBase>();     // 预览游戏物体字典
 
     public void Init() {
@@ -31,9 +34,19 @@ public class BuildManager : SingletonMono<BuildManager>
             previewBuilding.InitOnPreview();
             buildPreviewDict.Add(prefab.name, previewBuilding);
         }
-        // TOOD: 预览物体跟随鼠标, 跟随时物体需要在格子内
-        // TODO: 碰撞检测, 如果可以建造, 材质球设置为绿色, 否则为红色
-        // TODO: 检查是否进行建造, 确定建造需要判断物品是否满足要求
-        yield return null;
+
+        // 预览物体跟随鼠标, 跟随时物体需要在格子内
+        while (true) {
+            if (InputManager.Instance.GetMousePositionOnGround(Input.mousePosition, out Vector3 worldPosition)) {
+                // 将鼠标坐标转化为虚拟格子坐标, 需要对格子坐标进行取整
+                Vector3 virtualCellPosition = worldPosition;
+                virtualCellPosition.x = Mathf.RoundToInt(worldPosition.x / virtualCellSize) * virtualCellSize;
+                virtualCellPosition.z = Mathf.RoundToInt(worldPosition.z / virtualCellSize) * virtualCellSize;
+                previewBuilding.transform.position = virtualCellPosition;
+            }
+            // TODO: 碰撞检测, 如果可以建造, 材质球设置为绿色, 否则为红色
+            // TODO: 检查是否进行建造, 确定建造需要判断物品是否满足要求
+            yield return null;
+        }   
     }
 }
--- Assets/Scripts/GameScene/Building/BuildingBase.cs
+++ Assets/Scripts/GameScene/Building/BuildingBase.cs
@@ -7,9 +7,36 @@ public class BuildingBase : MapObjectBase
     [SerializeField] protected new Collider collider;
     public Collider Collider { get => collider; }
 
+    #region 预览模式
+    // 预览默认颜色红色和绿色
+    public static Color Red = new Color(1, 0, 0, 0.5f);
+    public static Color Green = new Color(0, 1, 0, 0.5f);
+    private MeshRenderer[] meshRenderers = null;
+    private List<Material> materialList = null;
+
     // 初始化预览方法
     public virtual void InitOnPreview() {
         // 预览时关闭碰撞体, 如果开启的话可能会存在问题
         collider.enabled = false;
-    }   
+        // 初始化材质球和MeshRender
+        meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        materialList = new List<Material>();
+        for (int i = 0; i < meshRenderers.Length; i++) {
+            materialList.AddRange(meshRenderers[i].materials);
+        }
+        for (int i = 0; i < materialList.Count; i++) {
+            // 默认为红色
+            materialList[i].color = Red;
+            // 设置成支持更改透明通道的rendering mode
+            ProjectTool.SetMaterialRenderingMode(materialList[i], ProjectTool.RenderingMode.Fade);
+        }
+    }
+
+    // 设置预览颜色
+    public void SetColorOnPreview(bool isRed) {
+        for (int i = 0; i < materialList.Count; i++) {
+            materialList[i].color = isRed == true ? Red : Green;
+        }
+    }
+    #endregion
 }
