64f211c26c84d5ce88d70e925b5224a5b9f19f50
ZhangGuoxin <warobot1997@gmail.com>
2023-09-11 11:25:34 +0800

    Block other UIs while building

--- Assets/Scripts/GameScene/BuildManager.cs
+++ Assets/Scripts/GameScene/BuildManager.cs
@@ -28,6 +28,8 @@ public class BuildManager : SingletonMono<BuildManager>
     IEnumerator DoBuildBuilding(BuildConfig buildConfig) {
         // 关闭鼠标与地图对象交互功能
         InputManager.Instance.SetCheckState(false);
+        // 关闭游戏UI交互功能
+        UIManager.Instance.DisableUIRaycaster();
         // 生成预览物体
         GameObject prefab = ConfigManager.Instance.GetConfig<MapObjectConfig>(ConfigName.MapObject, buildConfig.targetId).prefab;
         if (buildPreviewDict.TryGetValue(prefab.name, out BuildingBase previewBuilding) == false) {
@@ -35,9 +37,20 @@ public class BuildManager : SingletonMono<BuildManager>
             previewBuilding.InitOnPreview();
             buildPreviewDict.Add(prefab.name, previewBuilding);
         }
-
+        // 开启预览预制体
+        previewBuilding.gameObject.SetActive(true);
         // 预览物体跟随鼠标, 跟随时物体需要在格子内
         while (true) {
+            // 检查是否取消建造(鼠标右键)
+            if (Input.GetMouseButtonDown(1)) {
+                // 关闭预览物体
+                previewBuilding.gameObject.SetActive(false);
+                // 开启鼠标与地图对象交互功能
+                InputManager.Instance.SetCheckState(true);
+                // 开启游戏UI交互功能
+                UIManager.Instance.EnableUIRaycaster();
+                yield break;
+            }
             if (InputManager.Instance.GetMousePositionOnGround(Input.mousePosition, out Vector3 worldPosition)) {
                 // 将鼠标坐标转化为虚拟格子坐标, 需要对格子坐标进行取整
                 Vector3 virtualCellPosition = worldPosition;
@@ -80,8 +93,21 @@ public class BuildManager : SingletonMono<BuildManager>
                 previewBuilding.SetColorOnPreview(true);
             } else {
                 previewBuilding.SetColorOnPreview(false);
+                // 建造建筑物并移除对应资源
+                if (Input.GetMouseButtonDown(0)) {
+                    // 关闭预览物体
+                    previewBuilding.gameObject.SetActive(false);
+                    // 开启鼠标与地图对象交互功能
+                    InputManager.Instance.SetCheckState(true);
+                    // 开启游戏UI交互功能
+                    UIManager.Instance.EnableUIRaycaster();
+                    // 放置建筑物
+                    MapManager.Instance.GenerateMapObject(buildConfig.targetId, previewBuilding.transform.position);              
+                    // 根据建造配置减少背包中的物品
+                    UI_InventoryWindow.Instance.UpdateItemsForBuild(buildConfig);
+                    yield break;
+                }
             }
-            // TODO: 检查是否进行建造, 确定建造需要判断物品是否满足要求
             yield return null;
         }   
     }
